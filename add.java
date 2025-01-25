4e371c5e86f5b4da57d5aa3c1a6bac0a2f63c4de
Subhanandhini-760 <Subhanandhinivenkatesan@gmail.com>
2025-01-25 12:50:02 +0530

    Update add.java

--- add.java
+++ add.java
@@ -4,9 +4,9 @@ class add
 public static void main(String args[])
 {
 Scanner myobj = new Scanner(System.in);
-System.out.print("Enter first number:");
+System.out.print("Enter 1st number:");
 int firstNumber=myobj.nextInt();
-System.out.print("Enter second number:");
+System.out.print("Enter 2nd number:");
 int secondNumber=myobj.nextInt();
 System.out.println("First number:"+firstNumber);
 System.out.print("second number:"+secondNumber);
