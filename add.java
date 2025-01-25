550da80727d4099fe3bb6b9d974ba0c0d409e5eb
SUBHANANDHINI-0414 <subhanandhinivenkatesan@gmail.com>
2025-01-25 12:38:58 +0530

    push

---
+++ add.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class add
+{
+public static void main(String args[])
+{
+Scanner myobj = new Scanner(System.in);
+System.out.print("Enter first number:");
+int firstNumber=myobj.nextInt();
+System.out.print("Enter second number:");
+int secondNumber=myobj.nextInt();
+System.out.println("First number:"+firstNumber);
+System.out.print("second number:"+secondNumber);
+}
+}
